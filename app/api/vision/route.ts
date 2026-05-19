bad75628c3b71015e7fb4a8ceb1e4ae005ac6445
AdiNugroho <alexiusnicholas@rocketmail.com>
2026-05-19 20:03:32 +0700

    change model gemini

--- app/api/vision/route.ts
+++ app/api/vision/route.ts
@@ -13,7 +13,7 @@ export async function POST(req: Request) {
     const imageBytes = await req.arrayBuffer();
     const base64 = Buffer.from(imageBytes).toString("base64");
 
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
     const result = await model.generateContent([
       {
